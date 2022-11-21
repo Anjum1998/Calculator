3b8a3668d55cb13b651f126b3d3f1422f277cbb3
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-21 14:49:22 +0530

    updated

--- src/Calculator.java
+++ src/Calculator.java
@@ -34,8 +34,18 @@ public class Calculator {
                 int mul=a*b;
                 System.out.println("a * b ="+mul);
             case 4:
-                int div=a/b;
-                System.out.println("a / b ="+div);
+                try{
+                    int div=a/b;
+                    System.out.println("a / b ="+div);
+                }
+                catch (ArithmeticException e)
+                {
+                    System.out.println(" Exception b is Zero");
+                }
+                finally {
+                    System.out.println("pgm reached the line");
+                }
+
             case 5:
                 break;
             default:
