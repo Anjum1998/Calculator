60716ef277e30efd2579ff0299214b8cd37ba558
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-21 10:35:51 +0530

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first Number");
+        int a=sc.nextInt();
+        System.out.println("Enter second Number");
+        int b=sc.nextInt();
+
+        System.out.println("Enter the Choice:");
+
+        System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division");
+        int option=sc.nextInt();
+        switch (option)
+        {
+
+        }
+    }
 }
